7450b7eef0eb6055a4b6ae111b871fedfef4ec9c
Matteo Beccari <ma77eo0405@gmail.com>
2025-09-30 18:09:49 +0200

    stampa il risultato di un espressione

---
+++ tutorato/tutorato_1/espressione.c
@@ -0,0 +1,15 @@
+/*
+Esercizio 3 
+1.All'interno della cartella Tutorato_1, creare un file chiamato espressione.c.
+2.Aprire la cartella Tutorato_1 in Visual Studio Code utilizzando il comando code;
+3.Scrivere un programma in C nel file espressione.cche valuti e stampi a terminale il risultato della seguente espressione: 3×(4+2)−5 ;
+4.Compilare il sorgente creando un eseguibile chiamato valuta_espressione:
+5.Eseguire il programma dal terminale
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    printf("Risultato dell'espressione: %d\n",3*(4+2)-5);
+}    
