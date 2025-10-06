4125ed576b51132baf1e4402f8e3eb9e21fd04dd
Matteo Beccari <ma77eo0405@gmail.com>
2025-10-06 20:15:18 +0200

    rubrica con scelta

---
+++ controllo_di_flusso/salti/rubrica.c
@@ -0,0 +1,21 @@
+/*
+Scrivere un programma che simuli la scelta del numero da chiamare in una rubrica
+telefonica. In particolare, il programma deve
+- 1 stampare l’elenco dei contatti, ognuno preceduto da un identificativo intero
+    progressivo
+- 2 richiedere all’utente l’identificativo del contatto da chiamare
+- 3 stampare un messaggio che indichi che la chiamata al contatto 
+    selezionato è in corso.
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int scelta;
+    printf("1) Tizio 335-1234567\n");
+    printf("2) Caio 347-1234567\n");
+    printf("Scegliere il contatto: ");
+    scanf("%d", &scelta);
+    printf("Chiamata del contatto %d in corso\n", scelta);
+}
