5077e041bfb50475580b60533cf7ac1bdb48d316
Matteo Beccari <ma77eo0405@gmail.com>
2025-10-08 11:30:03 +0200

    dato un numero da 1 a 7 il programma stamperà il giorno della settimana corrispondente

--- controllo_di_flusso/istruzioni_condizionali/giorno.c
+++ controllo_di_flusso/istruzioni_condizionali/giorno.c
@@ -12,32 +12,40 @@ int main()
     printf("Inserire il numero del giorno: ");
     scanf("%d", &giorno);
 
-    if(giorno == 1)
+    if(giorno > 0  && giorno <8)
     {
-        printf("Lunedì\n");
+        if(giorno == 1)
+        {
+            printf("Lunedì\n");
+        }
+        else if(giorno == 2)
+        {
+            printf("Martedì\n");
+        }
+        else if(giorno == 3)
+        {
+            printf("Mercoledì\n");
+        }
+        else if(giorno == 4)
+        {
+            printf("Giovedì\n");
+        }
+        else if(giorno == 5)
+        {
+            printf("Venerdì\n");
+        }
+        else if(giorno == 6)
+        {
+            printf("Sabato\n");
+        }
+        else if(giorno == 7)
+        {
+            printf("Domenica\n");
+        }
     }
-    else if(giorno == 2)
+    else
     {
-        printf("Martedì\n");
+        printf("Il numero inserito non è corretto\n");
     }
-    else if(giorno == 3)
-    {
-        printf("Mercoledì\n");
-    }
-    else if(giorno == 4)
-    {
-        printf("Giovedì\n");
-    }
-    else if(giorno == 5)
-    {
-        printf("Venerdì\n");
-    }
-    else if(giorno == 6)
-    {
-        printf("Sabato\n");
-    }
-    else if(giorno == 7)
-    {
-        printf("Domenica\n");
-    }
+    
 }
