e3032348c3664dcac438c3265912faa1b2e34e7c
Matteo <ma77eo0405@gmail.com>
2025-10-09 15:18:49 +0200

    inserendo un mese e un anno restituisce i giorni di quel mese in quel determinato anno

---
+++ controllo_di_flusso/istruzioni_condizionali/giorni_mese.c
@@ -0,0 +1,35 @@
+/*
+Scrivere un programma che richieda all’utente mese (in formato numerico: 1 per
+gennaio, 12 per dicembre) e anno e stampi il numero di giorni di quel mese in
+quell’anno.
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int mese;
+    int anno;
+
+    printf("Inserisci il mese: ");
+    scanf("%d", &mese);
+    printf("Inserisci l'anno: ");
+    scanf("%d", &anno);
+
+    if (mese == 4 || mese == 6 || mese == 9 || mese == 11)
+    {
+        printf("Il mese %d/%d ha 30 giorni\n", mese, anno);
+    }
+    else if (mese == 2 && anno % 4 == 0 && anno % 100 != 0 || mese == 2 && anno % 400 == 0)
+    {
+        printf("Il mese %d/%d ha 29 giorni\n", mese, anno);
+    }
+    else if (mese == 2 && anno % 4 != 0 && anno % 100 == 0 || mese == 2 && anno % 400 != 0)
+    {
+        printf("Il mese %d/%d ha 28 giorni\n", mese, anno);
+    }
+    else
+    {
+        printf("Il mese %d/%d ha 31 giorni\n", mese, anno);
+    }
+}
