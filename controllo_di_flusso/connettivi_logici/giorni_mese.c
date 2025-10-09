e408ee63454e78ff35fe2aeb8c626120292f5141
Matteo <ma77eo0405@gmail.com>
2025-10-09 15:31:15 +0200

    dato un mese e un anno il programma restituisce il numero dei giorni di quel determinato anno

--- controllo_di_flusso/connettivi_logici/giorni_mese.c
+++ controllo_di_flusso/connettivi_logici/giorni_mese.c
@@ -11,25 +11,34 @@ int main()
     int mese;
     int anno;
 
+    input:
     printf("Inserisci il mese: ");
     scanf("%d", &mese);
     printf("Inserisci l'anno: ");
     scanf("%d", &anno);
 
-    if (mese == 4 || mese == 6 || mese == 9 || mese == 11)
+    if (mese >= 1 && mese <= 12)
     {
-        printf("Il mese %d/%d ha 30 giorni\n", mese, anno);
-    }
-    else if (mese == 2 && anno % 4 == 0 && anno % 100 != 0 || mese == 2 && anno % 400 == 0)
-    {
-        printf("Il mese %d/%d ha 29 giorni\n", mese, anno);
-    }
-    else if (mese == 2 && anno % 4 != 0 && anno % 100 == 0 || mese == 2 && anno % 400 != 0)
-    {
-        printf("Il mese %d/%d ha 28 giorni\n", mese, anno);
+        if (mese == 4 || mese == 6 || mese == 9 || mese == 11)
+        {
+            printf("Il mese %d/%d ha 30 giorni\n", mese, anno);
+        }
+        else if (mese == 2 && anno % 4 == 0 && anno % 100 != 0 || mese == 2 && anno % 400 == 0)
+        {
+            printf("Il mese %d/%d ha 29 giorni\n", mese, anno);
+        }
+        else if (mese == 2 && anno % 4 != 0 && anno % 100 == 0 || mese == 2 && anno % 400 != 0)
+        {
+            printf("Il mese %d/%d ha 28 giorni\n", mese, anno);
+        }
+        else
+        {
+            printf("Il mese %d/%d ha 31 giorni\n", mese, anno);
+        }
     }
     else
     {
-        printf("Il mese %d/%d ha 31 giorni\n", mese, anno);
+        printf("Il mese inserito non è corretto\n");
+        goto input;
     }
 }
