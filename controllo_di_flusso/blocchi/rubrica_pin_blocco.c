4625f2d954a4c2fe68f98a42f6f76f39a3bc0285
Matteo <ma77eo0405@gmail.com>
2025-10-09 11:03:44 +0200

    rubrica eseguita con if-else con blocco

---
+++ controllo_di_flusso/blocchi/rubrica_pin_blocco.c
@@ -0,0 +1,33 @@
+/*
+Scrivere un programma che simuli la scelta del numero da chiamare in una rubrica
+telefonica. In particolare, il programma deve
+- 1 stampare l’elenco dei contatti, ognuno preceduto da un identificativo intero
+    progressivo
+- 2 richiedere all’utente l’identificativo del contatto da chiamare
+- 3 stampare un messaggio che indichi che la chiamata al contatto 
+    selezionato è in corso.
+Fare in modo che il programma inizialmente chieda all’utente un PIN, ed esegua le
+successive istruzioni solo se il PIN immesso è uguale a 44122.
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int pin, scelta;
+    printf("Inserire PIN: ");
+    scanf("%d", &pin);
+
+    if(pin == 44122)
+    {
+        printf("1) Tizio 335-1234567\n");
+        printf("2) Caio 347-1234567\n");
+        printf("Scegliere il contatto: ");
+        scanf("%d", &scelta);
+        printf("Chiamata del contatto %d in corso\n", scelta);
+    }
+    else
+    {
+        printf("PIN Errato\n");
+    }
+}
