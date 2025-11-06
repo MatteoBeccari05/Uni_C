50c7d3f51c67501fa87a92098973aece6b062c79
Matteo <ma77eo0405@gmail.com>
2025-11-06 14:03:42 +0100

    chiede all'utente dei numeri e inserisce in un altro array solo i positivi, stampandoli

---
+++ array/positivi.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#define DIM 5
+
+int main()
+{
+    int a[DIM], b[DIM], dl = 0;
+
+    for(int i= 0; i<DIM; i++)
+    {
+        printf("Inserire numero %d: ", (i+1));
+        scanf("%d", &a[i]);
+    }
+
+    for(int i = 0; i < DIM; i++)
+    {
+        if(a[i] > 0)
+        {
+            b[dl]=a[i];
+            dl++;
+        }
+    }
+
+    for(int i = 0; i<dl; i++)
+    {
+        printf("%d\n", b[i]);
+    }
+}
