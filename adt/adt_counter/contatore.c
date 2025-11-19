7ee3463055be97ab34b8df005c521f91c8e7818d
Matteo <ma77eo0405@gmail.com>
2025-11-19 18:12:43 +0100

    adt e makefile con counter

---
+++ adt/adt_counter/contatore.c
@@ -0,0 +1,16 @@
+#include "contatore.h"
+
+void reset(Contatore *pc)
+{
+    (*pc).valore = 0;
+    (*pc).conta_operazioni++;
+}
+void inc(Contatore *pc)
+{
+    (*pc).valore++;
+    (*pc).conta_operazioni++;
+}
+unsigned int val(Contatore c)
+{
+    return c.valore;
+}
