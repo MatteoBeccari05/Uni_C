64a6dd433fffe7cd6b4c26390c9f4f309978c197
Matteo <ma77eo0405@gmail.com>
2025-11-20 10:35:13 +0100

    prova di makefile

---
+++ make/file1.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "file3.h"
+void p(int i)
+{
+    printf("%d\n", f(i));
+}
---
+++ make/file2.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "file3.h"
+void q(char i)
+{
+    printf("%c\n", f(i));
+}
